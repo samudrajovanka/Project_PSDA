1e349ab043f0079fba3f0234d20771b9eaf3d55e
Jovanka <samudrajovanka20@gmail.com>
2020-05-26 00:04:22 +0700

    update nama kelompok

--- Main.py
+++ Main.py
@@ -1,4 +1,8 @@
 '''
 Nama Kelompok:
-
+- Marshall Anugrah Najmi    1910511034
+- Jovanka Samudra           1910511040
+- Daffy Fayyadhya Ramzy     1910511044
+- Muhammad Raffiza Azka     1910511062
+- Brahma Aditama            19105110xx
 '''
