7fc1e3f71a7c3c57e36fc6dfb7fbd2d496b57fa3
Jovanka <51015175+samudrajovanka@users.noreply.github.com>
2020-05-26 19:28:59 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 '''
 Nama Kelompok:
 - Marshall Anugrah Najmi    1910511034
+- Brahma Aditama            1910511036
 - Jovanka Samudra           1910511040
 - Daffy Fayyadhya Ramzy     1910511044
 - Muhammad Raffiza Azka     1910511062
-- Brahma Aditama            19105110xx
 '''
